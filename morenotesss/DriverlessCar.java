56b1b52b546259a9c11e665ddae39223cf7db8db
Alexis-Inc <91219016+Alexis-Inc@users.noreply.github.com>
2022-03-01 12:01:00 -0800

    Add files via upload

---
+++ morenotesss/DriverlessCar.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class DriverlessCar here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface DriverlessCar
+{
+    public void move();
+    public void moveBackward();
+    
+}
